a6b3ee6eaeb037fb017d4ede9986bdf02c7b90ec
tomi204 <tomi204@outlook.com>
2023-01-21 14:06:21 -0300

    create user function

--- src/tradeCentral.sol
+++ src/tradeCentral.sol
@@ -1,42 +1,60 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract TradeCentral{
-     address public owner;
+contract TradeCentral {
 
-     uint256 itemCount;
+     //@dev global variables
+    address public owner;
 
-     struct Trade{
+    uint256 userCount;
+    uint256 tradeCount;
+
+//@dev structs
+    struct Trade {
         uint256 id;
-         address buyer;
-         address seller;
-         uint256 price;
-         string name;
-         string description;
-         string image;
-       }
+        address buyer;
+        address seller;
+        uint256 price;
+        string name;
+        string description;
+        string image;
+    }
 
-     struct userData{
-            address user;
-            Trade[] trades;
-            string email;
-            string name;
-            string image;
-     }
+    struct userData {
+        uint256 id;
+        address user;
+        Trade[] trades;
+        string email;
+        string name;
+        string image;
+    }
 
-     mapping(address => userData) public users;
-     mapping(uint256 => Trade) public trades;
+    mapping(uint256 => userData) public users;
+    mapping(uint256 => Trade) public trades;
 
-     constructor(){
-         owner = msg.sender;
-     }
+//@dev constructor
+    constructor() {
+        owner = msg.sender;
+    }
 
-     function createUser(string memory _email, string memory _name, string memory image)  external  {
-          require(msg.sender != address(0), "Invalid address");
-          require(users[msg.sender].user == msg.sender, "User already exists");
-          
-     }
-   
-
-
+//@dev function  create one userr
+    function createUser(
+        string memory _email,
+        string memory _name,
+        string memory image
+    ) external {
+        require(msg.sender != address(0), "Invalid address");
+        require(bytes(_email).length > 0, "Invalid email");
+        require(bytes(_name).length > 0, "Invalid name");
+        require(bytes(image).length > 0, "Invalid image");
+        userCount++;
+        users[userCount] = userData(
+            userCount,
+            msg.sender,
+            new Trade[](0),
+            _email,
+            _name,
+            image
+        );
+    }
 }
