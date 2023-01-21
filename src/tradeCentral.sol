852250bd446936bd2b0cda97f59b9c4b3be50d72
tomi204 <tomi204@outlook.com>
2023-01-21 13:56:44 -0300

    creando struct y instalando dependencias

--- src/tradeCentral.sol
+++ src/tradeCentral.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.13;
 
 contract TradeCentral{
      address public owner;
+
      uint256 itemCount;
+
      struct Trade{
         uint256 id;
          address buyer;
@@ -12,7 +14,7 @@ contract TradeCentral{
          string name;
          string description;
          string image;
-     }
+       }
 
      struct userData{
             address user;
@@ -20,9 +22,20 @@ contract TradeCentral{
             string email;
             string name;
             string image;
-            
-                
-                 }
+     }
+
+     mapping(address => userData) public users;
+     mapping(uint256 => Trade) public trades;
+
+     constructor(){
+         owner = msg.sender;
+     }
+
+     function createUser(string memory _email, string memory _name, string memory image)  external  {
+          require(msg.sender != address(0), "Invalid address");
+          require(users[msg.sender].user == msg.sender, "User already exists");
+          
+     }
    
 
 
